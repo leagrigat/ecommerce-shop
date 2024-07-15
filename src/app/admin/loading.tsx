6d11e0081203f031cd413718fb16a9a5b0d216d7
Lea Grigat <leagrigat@gmail.com>
2024-07-15 19:52:30 +0200

    fix: solved loading spinner error

--- src/app/admin/loading.tsx
+++ src/app/admin/loading.tsx
@@ -2,6 +2,6 @@ import { Loader2 } from "lucide-react";
 
 export default function AdminLoading() {
   <div className="flex justify-center">
-    <Loader2 className="size-24 animate spin" />
+    <Loader2 className="size-24 animate-spin" />
   </div>;
 }
