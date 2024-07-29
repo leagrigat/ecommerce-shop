7a034565c2451bd422e2be22ac154b7876e9edb5
Lea Grigat <leagrigat@gmail.com>
2024-07-29 16:55:54 +0200

    feat: elevate delete option within dropdown menu

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -16,6 +16,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import {
@@ -104,6 +105,7 @@ async function ProductsTable() {
                     id={product.id}
                     isAvailableForPurchase={product.isAvailableForPurchase}
                   />
+                  <DropdownMenuSeparator />
                   <DeleteDropdownItem
                     id={product.id}
                     disabled={product._count.orders > 0}
